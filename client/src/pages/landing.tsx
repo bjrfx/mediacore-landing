65d3c1b46dc78a76e44cf21c71daab735d03e2ae
bjrfx <phanikiran1234@gmail.com>
2025-12-13 20:20:18 +0530

    Adjust HeroSection bounce indicator margin
    Added an inline style to the bounce indicator in HeroSection to set marginBottom to '-2%'. This change fine-tunes the vertical positioning of the animated scroll indicator on the landing page.

--- client/src/pages/landing.tsx
+++ client/src/pages/landing.tsx
@@ -131,7 +131,7 @@ function HeroSection() {
         </motion.div>
       </div>
 
-      <div className="absolute bottom-8 left-1/2 -translate-x-1/2 animate-bounce">
+      <div className="absolute bottom-8 left-1/2 -translate-x-1/2 animate-bounce " style={{marginBottom: '-2%'}}>
         <div className="w-6 h-10 rounded-full border-2 border-white/30 flex items-start justify-center p-2">
           <div className="w-1.5 h-3 bg-white/50 rounded-full" />
         </div>
